00a939e29ffd5cae9f6e41a4300f65a8ae002066
MichalGurin <michal.gurin@gmail.com>
2020-05-01 19:15:11 +0200

    Migrated code files from Razor prototype

---
+++ DeepTargeting/Models/Interest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FBTargeting.Models
+{
+    public class Interest
+    {
+        public string Name { get; set; }
+        public string AudienceSize { get; set; }
+        public string Category { get; set; }
+        public string GoogleSearchUrl { get; set; }
+
+        public Interest()
+        {
+
+        }
+
+        public Interest(string name, string audienceSize, string category, string googleSearchUrl)
+        {
+            Name = name;
+            AudienceSize = audienceSize;
+            Category = category;
+            GoogleSearchUrl = googleSearchUrl;
+        }
+    }
+}
