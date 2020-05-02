3bf117b39e82eec7ed968b89fe3d354b89261baf
MichalGurin <michal.gurin@gmail.com>
2020-05-02 16:43:38 +0200

    Registration and login working

--- DeepTargeting/Startup.cs
+++ DeepTargeting/Startup.cs
@@ -36,7 +36,17 @@ namespace DeepTargeting
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-            
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = true;
+                options.Password.RequiredLength = 6;
+                options.Password.RequiredUniqueChars = 1;
+            });
+
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
