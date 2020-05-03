97ed676e076c78fa3db0aeb779f79f8f38b61135
MichalGurin <michal.gurin@gmail.com>
2020-05-03 12:36:15 +0200

    Footer "To the top" working on every View

--- DeepTargeting/Services/FBInterestsQueryService.cs
+++ DeepTargeting/Services/FBInterestsQueryService.cs
@@ -51,19 +51,6 @@ namespace DeepTargeting.Services
             return foundInterests;
         }
 
-        private string GetRequest(string uri)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
         private async Task<string> GetRequestAsync(string uri)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
