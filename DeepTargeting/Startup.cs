85f7648e517ef48cb2ae785d2ece7c3b512db796
MichalGurin <michal.gurin@gmail.com>
2020-05-17 12:35:57 +0200

    App now uses MS Sql Server DB

--- DeepTargeting/Startup.cs
+++ DeepTargeting/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using DeepTargeting.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using DeepTargeting.Services;
+
 
 namespace DeepTargeting
 {
@@ -62,9 +57,9 @@ namespace DeepTargeting
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
