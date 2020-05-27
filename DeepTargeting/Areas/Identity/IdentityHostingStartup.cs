96d832360367fe90d6472e05261fb3f98fd3d167
MichalGurin <michal.gurin@gmail.com>
2020-05-27 22:17:20 +0200

    Scaffolded all identity pages. Customized email confirmation page

---
+++ DeepTargeting/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using DeepTargeting.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DeepTargeting.Areas.Identity.IdentityHostingStartup))]
+namespace DeepTargeting.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
